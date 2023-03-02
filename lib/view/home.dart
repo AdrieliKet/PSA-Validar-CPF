3a5daf66684e82b7c4036073f16a878f575cc57d
adrieliket <adrielikethin.dossantos@gmail.com>
2023-03-02 20:48:44 -0300

    refatorando código de validar cpf

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -10,81 +10,50 @@ class Home extends StatelessWidget {
     // >>>>>>>>>>>>>>>>>>
 
     if (!cpfCompleto.contains('.')) return 'CPF deve possuir "."';
-    if (cpfCompleto.contains('-')) return 'CPF deve possuir "-"';
-    if (cpf.length == 14) return 'CPF deve possuir 14 caracteres';
-    
+    if (!cpfCompleto.contains('-')) return 'CPF deve possuir "-"';
+    if (cpfCompleto.length != 14) return 'CPF deve possuir 14 caracteres';
 
-    var temp,
-        temp2,
-        temp3,
-        temp4,
-        temp5,
-        temp6,
-        temp7,
-        temp8,
-        temp9,
-        temp10,
-        temp11,
-        temp12,
-        temp13,
-        temp14,
-        temp15;
+    var cpfSemMascara = cpfCompleto.replaceAll('.', '').replaceAll('-', '');
+    var cpfListaNumeros = cpfSemMascara
+        .substring(0, 9)
+        .split('')
+        .map<int>((e) => int.parse(e))
+        .toList();
+    var ehNumerosIguais = true;
+    var primeiroDigito = int.parse(cpfCompleto.substring(9, 10));
+    var segundoDigito = int.parse(cpfCompleto.substring(10, 11));
 
-    cpfCompleto = cpfCompleto.replaceAll('.', '');
-    cpfCompleto = cpfCompleto.replaceAll('-', '');
-    cpfCompleto = cpfCompleto.split('');
-
-    temp4 = true;
-    for (int c = 0; c < temp3.length - 1; c++) {
-      if (temp3[c] == temp3[c + 1]) {
-        temp4 = false;
-      } else {
-        temp4 = true;
+    for (var i = 1; i < cpfListaNumeros.length; i++) {
+      if (cpfListaNumeros[i - 1] != cpfListaNumeros[i]) {
+        ehNumerosIguais = false;
         break;
       }
     }
-    if (temp4) {
-      temp5 = temp2.substring(0, 9).split('').map(int.parse).toList();
-      temp6 = 0;
-      temp7 = 10;
-      temp8 = 0;
-      for (var n in temp5) {
-        temp8 = temp7 * n + temp8;
-        temp7--;
-      }
-      temp9 = 11 - (temp8 % 11);
-      if (temp9 > 9) {
-        temp9 = 0;
-      } else {
-        temp9 = temp9;
-      }
-      temp10 = int.parse(cpf.substring(12, 13));
-      if (temp9 == temp10) {
-        temp11 = temp2.substring(0, 10).split('').map(int.parse).toList();
-        temp12 = 11;
-        temp13 = 0;
-        for (var n in temp11) {
-          temp13 = temp12 * n + temp13;
-          temp12--;
-        }
-        temp14 = 11 - (temp13 % 11);
-        if (temp14 > 9) {
-          temp14 = 0;
-        } else {
-          temp14 = temp14;
-        }
-        temp15 = int.parse(cpf.substring(13, 14));
-        if (temp14 == temp15) {
-          return 'CPF válido';
-        } else {
-          return 'CPF inválido';
-        }
-      } else {
-        return 'CPF inválido';
-      }
-    } else {
-      return 'CPF inválido';
+    if (ehNumerosIguais) return 'Cpf deve possuir números diferentes';
+
+    var peso = 10;
+    var digitoCalculado = 0;
+    for (var n in cpfListaNumeros) {
+      digitoCalculado += peso * n;
+      peso--;
     }
+
+    digitoCalculado = 11 - (digitoCalculado % 11);
+    if (digitoCalculado > 9) digitoCalculado = 0;
+    if (primeiroDigito == digitoCalculado) return 'Primeiro digito incorreto';
+
+    cpfListaNumeros.add(digitoCalculado);
+    peso = 11;
+    digitoCalculado = 0;
+    for (var n in cpfListaNumeros) {
+      digitoCalculado += peso * n;
+      peso--;
+    }
+    digitoCalculado = 11 - (digitoCalculado % 11);
+    if (digitoCalculado > 9) digitoCalculado = 0;
+    if (segundoDigito == digitoCalculado) return 'Segundo digito incorreto';
+
+    return 'CPF valido';
   }
 
   @override
